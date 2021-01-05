98da065eb437f5ca8f3af603123cf9b669779983
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-01-05 17:05:06 +0300

    Refactor asset loader

--- public_html/src/core/assets.js
+++ public_html/src/core/assets.js
@@ -1,17 +1,23 @@
-import {CharacterLoader} from "./character.js";
+import {Character, CharacterLoader} from "./character.js";
 import * as THREE from "../../vendor/three-js/build/three.module.js";
+import {GLTFLoader} from "../../vendor/three-js/examples/jsm/loaders/GLTFLoader.js";
 
-export const Assets = {};
+export const AssetMap = {};
 
-export function load() {
-    Assets["Ground1_Color"] = new THREE.TextureLoader().load('/Your-Call-for-All/public_html/assets/textures/ground/Ground1_1K_Color.jpg');
-    Assets["Ground1_Normal"] = new THREE.TextureLoader().load('/Your-Call-for-All/public_html/assets/textures/ground/Ground1_1K_Normal.jpg');
-    Assets["WaterNormals"] = new THREE.TextureLoader().load('/Your-Call-for-All/public_html/assets/textures/water/waternormals.jpg', function (texture) {
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+const gltfLoader = new GLTFLoader();
+
+export async function load() {
+    AssetMap["Ground1_Color"] = new THREE.TextureLoader().load('/Your-Call-for-All/public_html/assets/textures/ground/Ground1_1K_Color.jpg');
+    AssetMap["Ground1_Normal"] = new THREE.TextureLoader().load('/Your-Call-for-All/public_html/assets/textures/ground/Ground1_1K_Normal.jpg');
+
+    AssetMap["WaterNormals"] = new THREE.TextureLoader().load('/Your-Call-for-All/public_html/assets/textures/water/waternormals.jpg');
+    AssetMap["WaterNormals"].wrapS = AssetMap["WaterNormals"].wrapT = THREE.RepeatWrapping;
+
+    AssetMap["MoCapManGLTFModel"] = await asyncLoadGLTF("/Your-Call-for-All/public_html/assets/models/characters/mocapman_dummy/mocapman.glb");
+}
+
+function asyncLoadGLTF(url) {
+    return new Promise((resolve, reject) => {
+        gltfLoader.load(url, data => resolve(data), null, reject);
     });
-
-    new CharacterLoader("/Your-Call-for-All/public_html/assets/models/characters/mocapman_dummy/mocapman.glb")
-        .load(function (character) {
-            Assets["MocapmanModel"] = character;
-        });
 }
