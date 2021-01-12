5bd7ee6c4ae22647250a48b13ca5be87421e0e56
cagatayyigit <cagatayyigit3@gmail.com>
2021-01-12 12:08:52 +0300

    Refactor main.js

--- public_html/src/main.js
+++ public_html/src/main.js
@@ -4,10 +4,13 @@ import {EffectComposer} from "../vendor/three-js/examples/jsm/postprocessing/Eff
 import {RenderPass} from "../vendor/three-js/examples/jsm/postprocessing/RenderPass.js";
 import {Assets} from "./core/assets.js";
 import {createPerformanceMonitor} from "./util/debug.js";
+import {UnrealBloomPass} from "../vendor/three-js/examples/jsm/postprocessing/UnrealBloomPass.js";
 
 
-window.onload = function () {
-    init();
+let settings = {
+    useGridHelper: false,
+    useBloom: false,
+    usePerformanceMonitor: false,
 }
 
 
@@ -19,7 +22,6 @@ let stats;
 
 function init() {
 
-
     Assets.load(() => {
         removeLoadingBar();
         clock = new THREE.Clock();
@@ -27,38 +29,36 @@ function init() {
         initListeners();
         initScene();
         initRenderer();
-
-        composer = new EffectComposer(renderer);
-        composer.addPass(new RenderPass(scene, camera));
-        //composer.addPass(new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.5, 0.3, 0.95));
-
-
         yourCallForAll = new YourCallForAll(scene, camera);
         clock.start();
+        applySettings();
         render();
-
-        //const helper = new THREE.GridHelper(1000, 1000, 0xffffff, 0xffffff);
-        //helper.position.y = 1;
-        //scene.add(helper);
-
-
     });
-
-    stats = createPerformanceMonitor(document.body);
-
 }
 
 
 function render() {
     let deltaTime = clock.getDelta();
-    stats.update();
+    if (stats) {stats.update();}
+
     yourCallForAll.update(deltaTime);
     renderer.toneMappingExposure = yourCallForAll.environment.sky.props.exposure;
-
     composer.render();
-
-
     requestAnimationFrame(render);
+}
+
+function applySettings(){
+    if (settings.useBloom){
+        composer.addPass(new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.5, 0.3, 0.95));
+    }
+    if (settings.useGridHelper){
+        const helper = new THREE.GridHelper(1000, 1000, 0xffffff, 0xffffff);
+        helper.position.y = 1;
+        scene.add(helper);
+    }
+    if (settings.usePerformanceMonitor){
+        stats = createPerformanceMonitor(document.body);
+    }
 }
 
 function removeLoadingBar(){
@@ -83,13 +83,14 @@ function initRenderer() {
     renderer.setPixelRatio(1.0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     // For some reason, these break the water color
-
     //renderer.toneMapping = THREE.ACESFilmicToneMapping;
     //renderer.toneMappingExposure = 0.5;
-
     //renderer.outputEncoding = THREE.sRGBEncoding;
     //renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
 
     document.body.appendChild(renderer.domElement);
 }
@@ -99,10 +100,13 @@ function initScene() {
 
 }
 
-
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+window.onload = function () {
+    init();
 }
