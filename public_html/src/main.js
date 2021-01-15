47baf3ee6c0dc2beea5401bc58d9083589cd4cc8
cagatayyigit <cagatayyigit3@gmail.com>
2021-01-15 22:34:13 +0300

    Add simple settings menu

--- public_html/src/main.js
+++ public_html/src/main.js
@@ -25,7 +25,33 @@ function init() {
     const loadingElem = document.querySelector('#button');
     loadingElem.addEventListener("click", () => {
         flag *= -1;
+
+        const menuEl = document.querySelector('#menu');
+
+        if (flag === -1){
+            menuEl.style.visibility = 'hidden';
+            //abc.style.visibility = 'hidden';
+        }
+        else {
+            menuEl.style.visibility = 'visible';
+            //abc.style.visibility = 'visible';
+        }
+
     });
+
+    const resume = document.querySelector('#abc');
+    resume.addEventListener("click",()=>{
+        const menuEl = document.querySelector('#menu');
+        flag *= -1;
+        if (flag === -1){
+            menuEl.style.visibility = 'hidden';
+            //abc.style.visibility = 'hidden';
+        }
+        else {
+            menuEl.style.visibility = 'visible';
+            //abc.style.visibility = 'visible';
+        }
+    })
 
     Assets.load(() => {
         removeLoadingBar();
