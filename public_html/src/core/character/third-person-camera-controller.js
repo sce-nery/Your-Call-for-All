533c2c20efc6ccc02488ab662a50487260065ec3
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-01-15 22:54:53 +0300

    Refactor

--- public_html/src/core/character/third-person-camera-controller.js
+++ public_html/src/core/character/third-person-camera-controller.js
@@ -10,10 +10,14 @@ class ThirdPersonCameraController {
         this.currentLookAt = new THREE.Vector3();
 
         this.spherical = new THREE.Spherical(1,
-            THREE.Math.degToRad(45),
+            THREE.Math.degToRad(85),
             THREE.Math.degToRad(0),
-            );
+        );
 
+        this.boundaries = {
+            maxPolarAngle: THREE.Math.degToRad(115),
+            minPolarAngle: THREE.Math.degToRad(45)
+        }
 
         this.initializeListeners();
     }
@@ -21,12 +25,12 @@ class ThirdPersonCameraController {
     initializeListeners() {
         const self = this;
         this.mouseMoved = function (event) {
-            const sensitivity = 0.005;
+            const sensitivity = 0.0025;
             self.spherical.theta -= sensitivity * event.movementX;
             self.spherical.phi -= sensitivity * event.movementY;
 
-            const maxPolarAngle = THREE.Math.degToRad(135);
-            const minPolarAngle = THREE.Math.degToRad(45);
+            const maxPolarAngle = self.boundaries.maxPolarAngle;
+            const minPolarAngle = self.boundaries.minPolarAngle;
             self.spherical.phi = Math.max(minPolarAngle, Math.min(maxPolarAngle, self.spherical.phi));
 
         }
@@ -46,46 +50,57 @@ class ThirdPersonCameraController {
         });
     }
 
-    calculateIdealOffset() {
-        const idealOffset = new THREE.Vector3(0, 1.75, -4);
-        idealOffset.applyQuaternion(this.character.controller.locomotion.rotation);
-        idealOffset.add(this.character.controller.locomotion.position);
-        return idealOffset;
+    calculateCameraPosition() {
+        // Camera position relative to character.
+        const cameraPosition = new THREE.Vector3(-0.5, 1.75, -4);
+
+        // Apply rotation and position of the character to get world-space coords
+        cameraPosition.applyQuaternion(this.character.controller.locomotion.rotation);
+        cameraPosition.add(this.character.controller.locomotion.position);
+
+        return cameraPosition;
     }
 
-    calculateIdealLookAt() {
-        const idealLookAt = new THREE.Vector3(0, 1.0, 0);
-        idealLookAt.applyQuaternion(this.character.controller.locomotion.rotation);
-        idealLookAt.add(this.character.controller.locomotion.position);
-        return idealLookAt;
+    calculateFocus() {
+        // Look at point relative to character.
+        const focus = new THREE.Vector3(-0.5, 1.0, 0);
+
+        // Apply character's rotation and position to get world-space coords
+        focus.applyQuaternion(this.character.controller.locomotion.rotation);
+        focus.add(this.character.controller.locomotion.position);
+
+        return focus;
     }
 
     enterPointerLock() {
         this.character.owner.renderer.domElement.requestPointerLock();
     }
 
-    lookAround(eyePos, target) {
-        let targetToEye = eyePos.clone().sub(target);
+    lookAround(cameraPos, target) {
+        let targetToCamera = cameraPos.clone().sub(target);
 
-        this.spherical.radius = targetToEye.length();
-        targetToEye.setFromSpherical(this.spherical);
+        this.spherical.radius = targetToCamera.length();
+        targetToCamera.setFromSpherical(this.spherical);
 
-        eyePos.copy(targetToEye.clone().add(target));
+        cameraPos.copy(targetToCamera.clone().add(target));
     }
 
     update(deltaTime) {
-        const idealOffset = this.calculateIdealOffset();
-        const idealLookAt = this.calculateIdealLookAt();
+        const eyePosition = this.calculateCameraPosition();
+        const focus = this.calculateFocus();
 
-        this.lookAround(idealOffset, idealLookAt);
+        this.lookAround(eyePosition, focus);
 
         const t = 1.0 - Math.pow(0.001, deltaTime);
 
-        this.currentCameraPosition.lerp(idealOffset, t);
-        this.currentLookAt.lerp(idealLookAt, t);
+        this.currentCameraPosition.lerp(eyePosition, t);
+        this.currentLookAt.lerp(focus, t);
 
         this.camera.position.copy(this.currentCameraPosition);
         this.camera.lookAt(this.currentLookAt);
+    }
+
+    boundaryCheck() {
     }
 }
 
